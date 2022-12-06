e28b60be33648e2e092e7292421d13c08c16f67e
raghavlite <raghavlite@gmail.com>
2022-12-06 16:46:57 -0500

    minor

--- scripts/wmt30/spm_vocab_to_fairseq_dict.sh
+++
@@ -1,3 +0,0 @@
-SPM_VOCAB="/private/home/shru/wmt_datasets/spm_64000_400M.vocab"
-FAIRSEQ_DICT="/private/home/shru/wmt_datasets/spm_64000_400M.dict"
-tail -n +4 $SPM_VOCAB | awk '{print $1" "1}' > $FAIRSEQ_DICT
